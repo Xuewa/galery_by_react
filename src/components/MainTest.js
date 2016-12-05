060e7d692b238cccdadbd63e03a61f911aa91312
Xuewa <366653992@qq.com>
2016-12-05 23:15:35 +0800

    extends fun

--- src/components/MainTest.js
+++ src/components/MainTest.js
@@ -3,19 +3,18 @@ require('styles/App.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Rectangle from './rectangle';
 import PathEle from './PathEle';
-
-
 
 var GaleryByReactApp=React.createClass({
 
 	render:function() {
+    var wid=100,hei=100;
+    var x=0,y=10;
 
-			return (
-				//<section className="stage" ref="stage">
-					<PathEle className="path"/>
-
-				//</section>
+    return (
+					//<Rectangle  width={wid} height={hei} x={x} y={y}/>
+            <PathEle />
 	    	);
 	}
 });
